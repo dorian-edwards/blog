a8f08aaa8d5cbb99fc509da95a77966e28c454c3
Dorian Edwards <de.pharmd2018@gmail.com>
2021-07-02 21:58:27 -0400

    add img for preview, modify head meta tags

--- components/layout.js
+++ components/layout.js
@@ -9,12 +9,11 @@ const Layout = ({ children }) => {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta charSet='UTF-8' />
         <meta name='description' content='A website about me and journey to tech...' />
-        <meta property="og:image" content="https://avatars.githubusercontent.com/u/47185542?v=4?s=400" />
+        <meta property="og:image" content="/assets/images/bmore.png" />
         <meta property="og:image:alt" content="My personal website built with NextJS!" />
-        <meta property="og:site_name" content="GitHub" />
-        <meta property="og:type" content="profile" />
-        <meta property="og:title" content="buddafucofibas - Overview" />
-        <meta property="og:url" content="https://github.com/buddafucofibas" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="My personal website" />
+        <meta property="og:url" content="https://pharmdtechie.net" />
         <meta property="og:description" content="My personal website built with NextJS!" />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
