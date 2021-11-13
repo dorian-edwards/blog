92c93025e25791b23ef8b734664f469eef929abe
Dorian Edwards <pharmdtechie@Dorians-MBP.fios-router.home>
2021-11-13 09:46:45 -0500

    update meta-data

--- components/layout.js
+++ components/layout.js
@@ -8,14 +8,20 @@ const Layout = ({ children }) => {
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta charSet='UTF-8' />
-        <meta name='author' content='dorian edwards'/>
-        <meta name='description' content='A website about me and journey to tech...' />
-        <meta property="og:image" content="/assets/images/bmore.png" />
-        <meta property="og:image:alt" content="My personal website built with NextJS!" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="My personal website" />
-        <meta property="og:url" content="https://pharmdtechie.net" />
-        <meta property="og:description" content="My personal website built with NextJS!" />
+        <meta name='author' content='dorian edwards' />
+        <meta
+          name='description'
+          content='A website about me and journey to tech...'
+        />
+        <meta property='og:image' content='/assets/images/homepage.png' />
+        <meta property='og:image:alt' content='Home page of blog site.' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='PharmDTechie.net' />
+        <meta property='og:url' content='https://pharmdtechie.net' />
+        <meta
+          property='og:description'
+          content='My personal website built with NextJS!'
+        />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
           href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap'
